8d5abd8063742d45a05bfe0c964c29fdf43a7bd6
Christopher Bernhard <58096490+cbernhard21@users.noreply.github.com>
2021-10-15 21:34:49 -0500

    Delete wls-troubleshooting directory

--- wls-troubleshooting/js/app.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-import { getNavHtml } from './navHtml.js';
-
-getNavHtml();
--- wls-troubleshooting/js/navHtml.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-export async function getNavHtml() {
-    const response = await fetch('./components/nav.html');
-    const navHtml = await response.text();
-    document.querySelector('#nav').innerHTML = navHtml;
-}
