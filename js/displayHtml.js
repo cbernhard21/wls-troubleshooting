e2e3c1fcca677ae7f84b93370882b91a105f3f4b
Christopher Bernhard <58096490+cbernhard21@users.noreply.github.com>
2021-11-24 14:36:25 -0600

    Add files via upload

--- js/displayHtml.js
+++ js/displayHtml.js
@@ -1,19 +1,19 @@
 'use strict'
 
 export async function displayOverdriveHtml() {
-    const overdriveContainer = document.querySelector('.data-container');
-
+    //get data from the internal json file
     const response = await fetch('./db.json')
     const data = await response.json();
 
+    //global variables
+    const overdriveContainer = document.querySelector('.data-container');
+    const overdriveIpContainer = document.querySelector('.overdrive-ips');
     const overdriveData = data.services.overdrive;
-
-    let overdriveHtml = '';
-
+    const overdriveIps = data.IPaddresses;
 
     //loop to create each issue section
     let allOverdriveHtml = overdriveData.map(item => {
-        let stepsHtml = [];
+        let stepsHtml = '';
         let notes = item.notes
         let stepsArr = item.steps;
 
@@ -23,30 +23,47 @@ export async function displayOverdriveHtml() {
         }
 
         if (stepsArr) {
-            //loop for the steps to creat list item
-            for (let i = 0; i < stepsArr.length; i++) {
-                let stepItem = `<li class="details-solution-steps">${stepsArr[i]}</li>`;
-                stepsHtml.push(stepItem);
+            //loop for the steps to create list items
+
+            for (let step of stepsArr) {
+                let stepHtml = `<li class="details-solution-steps">${step}</li>`
+                stepsHtml += stepHtml;
             }
         } else {
-            stepsHtml = [];
+            stepsHtml = '';
         }
 
-        overdriveHtml = `
+        let overdriveHtml = `
             <details class="details">
             <summary class="details-issue pointer">${item.issue}</summary>
             <article class="details-solution">
               <h3 class="details-solution-title">${item.fix}</h3>
               <ol class="details-solution-list">
-                ${stepsHtml.join('')}
+                ${stepsHtml}
               </ol>
               <p class="details-notes">${notes}</p>
             </article>
           </details>
         `;
         return overdriveHtml;
-    })
+    }).join('');
     overdriveContainer.innerHTML = allOverdriveHtml;
+
+    //create overdrive IP HTML
+    let overdriveIpHtml = overdriveIps.map(item => {
+        let overdriveIpHtmlLine = `
+            <p class="flex flex-direction">
+              <span class="details-solution-steps w-300">${item.device}</span>
+              <span class="bold">${item.ip}</span>
+            </p>
+
+      `;
+        return overdriveIpHtmlLine;
+
+    }).join('');
+
+    overdriveIpContainer.innerHTML = overdriveIpHtml;
+
 }
 
 export async function displayVintenHtml() {
